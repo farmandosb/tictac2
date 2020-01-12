3244d45067aeaadd7b132c7dac0fc98c0a2a1a43
Freddy Suarez <freddy.armando.suarez@gmail.com>
2020-01-12 00:55:53 -0300

    Updates

---
+++ precache-manifest.fa8066b3b78e1ea4cdbbdddd3b06769a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d83edc64bad893f80362ec29bf6dd1",
+    "url": "/tictac2/index.html"
+  },
+  {
+    "revision": "72e44784e675152b5a41",
+    "url": "/tictac2/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e96b3869a805aeef29da",
+    "url": "/tictac2/static/js/2.dff762f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictac2/static/js/2.dff762f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72e44784e675152b5a41",
+    "url": "/tictac2/static/js/main.7c8bc47c.chunk.js"
+  },
+  {
+    "revision": "0a8b1c149c2761a67391",
+    "url": "/tictac2/static/js/runtime-main.9d1a4001.js"
+  }
+]);
